a35c78ca237f628c77040bc93a135822615a5cdb
Clayton Andrews <clay.j.andrews@gmail.com>
2017-07-21 00:23:36 -0400

    Actual Intial Commit (actual sample from GastricSparrow)
    Initital scripts, level, etc. provided by Gastric (just uploading in their stead since Git GUIs can be weird)

---
+++ Assets/Scipts/Controller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	Rigidbody rBody;
+
+	float forwardInput, turnInput;
+
+	Quaternion targetRotation;
+	public Quaternion TargetRotation {
+		get { return targetRotation; }
+	}
+
+
+	public float inputDelay = 0.1f;
+	public float forwardVel = 20f;
+	public float rotateVel = 100f;
+
+
+	void Start () {
+		// locate player
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody> ()) {
+			rBody = GetComponent<Rigidbody> ();
+		} else {
+			Debug.LogError ("No rigidbody");
+		}
+		forwardInput = turnInput = 0;
+	}
+
+	void GetInput() {
+		forwardInput = Input.GetAxis ("Vertical");
+		turnInput = Input.GetAxis ("Horizontal");
+	}
+
+		
+
+	void Update () {
+		GetInput ();
+		Turn ();
+	}
+
+	void FixedUpdate() {
+
+		Run ();
+	}
+
+	void Run() {
+		if (Mathf.Abs(forwardInput) > inputDelay) {
+			//move
+			rBody.velocity = transform.forward * forwardInput * forwardVel;
+		}
+		else {
+			//stop
+			rBody.velocity = Vector3.zero;
+		}
+	}
+
+	void Turn() {
+		if (Mathf.Abs (turnInput) > inputDelay) {
+			targetRotation *= Quaternion.AngleAxis (rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+	}
+
+
+}
+
